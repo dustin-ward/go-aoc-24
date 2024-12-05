a75217105da745c6619f77f893a04e81b0c7ff91
dustin-ward <wardddustin@gmail.com>
2024-12-04 21:30:23 -0500

    Day 1 Solution

--- day1/part1.go
+++ day1/part1.go
@@ -2,17 +2,42 @@ package day1
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"slices"
+	"strconv"
+
+	"github.com/dustin-ward/go-aoc-24/util"
 )
 
 func Part1(inpt io.Reader) string {
+    L := make([]int,0)
+    R := make([]int,0)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        var a,b int
+        fmt.Sscanf(line, "%d %d", &a, &b)
+
+        L = append(L, a)
+        R = append(R, b)
     }
+
+    slices.Sort(L)
+    slices.Sort(R)
+
+    ans := 0
+
+    for i := range len(L) {
+        ans += util.Abs(L[i]-R[i])
+    }
+
+    return strconv.Itoa(ans)
 }
--- day1/part2.go
+++ day1/part2.go
@@ -2,17 +2,36 @@ package day1
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 func Part2(inpt io.Reader) string {
+    L := make([]int,0)
+    R := make(map[int]int)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        var a,b int
+        fmt.Sscanf(line, "%d %d", &a, &b)
+
+        L = append(L, a)
+        R[b]++
     }
+
+    ans := 0
+
+    for _, x := range L {
+        ans += x * R[x]
+    }
+
+    return strconv.Itoa(ans)
 }
