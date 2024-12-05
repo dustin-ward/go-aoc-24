73e25751be1a389fddfa1beacbd3b322aeede37d
dustin-ward <wardddustin@gmail.com>
2024-12-05 01:13:03 -0500

    Day 5 Solutions

--- day5/part1.go
+++ day5/part1.go
@@ -2,17 +2,59 @@ package day5
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Part1(inpt io.Reader) string {
+    var ans int
+    rules := true
+
+    prereqs := make(map[int]map[int]bool)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        if line == "" {
+            rules = false
+            continue
+        }
+
+        if rules {
+            var a,b int
+            fmt.Sscanf(line, "%d|%d", &a, &b)
+            if prereqs[a] == nil {
+                prereqs[a] = make(map[int]bool)
+            }
+            prereqs[a][b] = true
+        } else {
+            var mid int
+            curPages := make(map[int]bool)
+            pages := strings.Split(line, ",")
+            for _, page := range pages {
+                pageInt, _ := strconv.Atoi(page)
+
+                for c, _ := range curPages {
+                    if _, ok := prereqs[pageInt][c]; ok {
+                        goto invalid
+                    }
+                }
+                
+                curPages[pageInt] = true
+            }
+            mid, _ = strconv.Atoi(pages[len(pages)/2])
+            ans += mid
+invalid:
+        }
 	}
+
+    return strconv.Itoa(ans)
 }
--- day5/part2.go
+++ day5/part2.go
@@ -2,17 +2,78 @@ package day5
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func Part2(inpt io.Reader) string {
+	var ans int
+	rules := true
+
+	prereqs := make(map[int]map[int]struct{})
+	rulesList := make([]string, 0)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+			break
 		}
 		line = line[:len(line)-1]
+
+		if line == "" {
+			rules = false
+			continue
+		}
+
+		if rules {
+			var a, b int
+			fmt.Sscanf(line, "%d|%d", &a, &b)
+			if prereqs[a] == nil {
+				prereqs[a] = make(map[int]struct{})
+			}
+			if prereqs[b] == nil {
+				prereqs[b] = make(map[int]struct{})
+			}
+			prereqs[a][b] = struct{}{}
+		} else {
+			rulesList = append(rulesList, line)
+		}
 	}
+
+    for _, rule := range rulesList {
+        s := strings.Split(rule, ",")
+        orig := make([]int,len(s))
+        sorted := make([]int,len(s))
+
+        for i, str := range s {
+            x, _ := strconv.Atoi(str)
+            orig[i] = x
+            sorted[i] = x
+        }
+
+        slices.SortStableFunc(sorted, func(i,j int) int {
+            if _, ok := prereqs[i][j]; ok {
+                return -1
+            } else if _, ok := prereqs[j][i]; ok {
+                return 1
+            }
+            return 0
+        })
+
+        for i, _ := range orig {
+            if orig[i] != sorted[i] {
+                goto good
+            }
+        }
+        continue
+good:
+        ans += sorted[len(sorted)/2]
+    }
+
+	return strconv.Itoa(ans)
 }
