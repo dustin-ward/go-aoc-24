5712685148150a090815c15a8fe7a47f1215ebe3
dustin-ward <wardddustin@gmail.com>
2024-12-04 21:35:07 -0500

    Day 3 Solution

--- day3/part1.go
+++ day3/part1.go
@@ -3,16 +3,28 @@ package day3
 import (
 	"bufio"
 	"io"
+	"regexp"
+	"strconv"
 )
 
 func Part1(inpt io.Reader) string {
-	reader := bufio.NewReader(inpt)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			//EOF
-			return "Not implemented"
-		}
-		line = line[:len(line)-1]
+    var ans = 0
+    r := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+
+    reader := bufio.NewReader(inpt)
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            //EOF
+            return strconv.Itoa(ans)
+        }
+        line = line[:len(line)-1]
+
+        for _, match := range r.FindAllStringSubmatch(line,-1) {
+            a, _ := strconv.Atoi(match[1])
+            b, _ := strconv.Atoi(match[2])
+            ans += a*b
+        }
     }
+
 }
--- day3/part2.go
+++ day3/part2.go
@@ -3,16 +3,85 @@ package day3
 import (
 	"bufio"
 	"io"
+	"regexp"
+	"sort"
+	"strconv"
 )
 
 func Part2(inpt io.Reader) string {
+	var ans = 0
+	rMul := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	rDo := regexp.MustCompile(`do\(\)`)
+	rDont := regexp.MustCompile(`don't\(\)`)
+
+	fullLine := ""
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+			break
 		}
 		line = line[:len(line)-1]
-    }
+
+		fullLine += line
+	}
+
+	type instrs struct {
+		pos      int
+		instType int
+		a        int
+		b        int
+	}
+	switches := make([]instrs, 0)
+
+	dos := rDo.FindAllStringIndex(fullLine, -1)
+	for _, do := range dos {
+		switches = append(switches, instrs{
+			do[0],
+			1,
+			0,
+			0,
+		})
+	}
+	donts := rDont.FindAllStringIndex(fullLine, -1)
+	for _, dont := range donts {
+		switches = append(switches, instrs{
+			dont[0],
+			-1,
+			0,
+			0,
+		})
+	}
+	for _, match := range rMul.FindAllStringSubmatchIndex(fullLine, -1) {
+		a, _ := strconv.Atoi(fullLine[match[2]:match[3]])
+		b, _ := strconv.Atoi(fullLine[match[4]:match[5]])
+		switches = append(switches, instrs{
+			match[0],
+			0,
+			a,
+			b,
+		})
+	}
+
+	sort.Slice(switches, func(i, j int) bool {
+		return switches[i].pos < switches[j].pos
+	})
+
+	state := true
+	for _, instr := range switches {
+		switch instr.instType {
+		case -1:
+			state = false
+		case 0:
+			if state {
+				ans += instr.a * instr.b
+			}
+		case 1:
+			state = true
+
+		}
+	}
+
+    return strconv.Itoa(ans)
 }
