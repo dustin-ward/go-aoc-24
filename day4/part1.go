d3b46e14ccc6db618ec9bc98943e99398c7dec8a
dustin-ward <wardddustin@gmail.com>
2024-12-04 21:42:55 -0500

    Day 4 Solution

--- day4/part1.go
+++ day4/part1.go
@@ -3,16 +3,53 @@ package day4
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part1(inpt io.Reader) string {
-	reader := bufio.NewReader(inpt)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			//EOF
-			return "Not implemented"
-		}
-		line = line[:len(line)-1]
+    var board []string
+
+    var dx = []int{-1,-1,0,1,1,1,0,-1}
+    var dy = []int{0,-1,-1,-1,0,1,1,1}
+
+    var n,m int
+
+    bounds := func (i,j int) bool {
+        return i>=0 && i<n && j>=0 && j<m
     }
+
+    const XMAS = "XMAS"
+
+    reader := bufio.NewReader(inpt)
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            //EOF
+            break
+        }
+        line = line[:len(line)-1]
+        board = append(board, line)
+    }
+
+    n = len(board)
+    m = len(board[0])
+
+    ans := 0
+    for i := range n {
+        for j := range m {
+            for k := range 8 {
+                for cnt, ch := range "XMAS" {
+                    i2 := i+(dy[k]*cnt)
+                    j2 := j+(dx[k]*cnt)
+                    if !bounds(i2,j2) || board[i2][j2] != byte(ch) {
+                        goto invalid
+                    }
+                }
+                ans++
+invalid:
+            }
+        }
+    }
+
+    return strconv.Itoa(ans)
 }
--- day4/part2.go
+++ day4/part2.go
@@ -3,16 +3,53 @@ package day4
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part2(inpt io.Reader) string {
-	reader := bufio.NewReader(inpt)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			//EOF
-			return "Not implemented"
-		}
-		line = line[:len(line)-1]
+    var board []string
+
+    var n,m int
+
+    reader := bufio.NewReader(inpt)
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            //EOF
+            break
+        }
+        line = line[:len(line)-1]
+        board = append(board, line)
     }
+
+    n = len(board)
+    m = len(board[0])
+
+    ans := 0
+    for i := range n {
+        for j := range m {
+            var chUL,chUR,chBR,chBL byte
+            var d1,d2 bool
+            if board[i][j] != 'A' || i == 0 || i == n-1 || j == 0 || j == m-1 {
+                goto invalid
+            }
+
+            chUL = board[i-1][j-1]
+            chBR = board[i+1][j+1]
+            chUR = board[i-1][j+1]
+            chBL = board[i+1][j-1]
+
+            d1 = (chUL == 'M' && chBR == 'S') || (chUL == 'S' && chBR == 'M')
+            d2 = (chUR == 'M' && chBL == 'S') || (chUR == 'S' && chBL == 'M')
+
+            if !(d1 && d2) {
+                goto invalid
+            }
+
+            ans++
+invalid:
+        }
+    }
+
+    return strconv.Itoa(ans)
 }
