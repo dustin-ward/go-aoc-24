60c3bb8912db3cc5c7ebbc8cd735a3d601d5c212
dustin-ward <wardddustin@gmail.com>
2024-12-09 01:04:05 -0500

    Day 9 Solutions

--- day9/part1.go
+++ day9/part1.go
@@ -3,16 +3,62 @@ package day9
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part1(inpt io.Reader) string {
 	reader := bufio.NewReader(inpt)
+    var ans int
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            return strconv.Itoa(ans)
 		}
 		line = line[:len(line)-1]
+
+        fs := make([]int,0)
+        id := 0
+        for i := 0; i<len(line); i+=2 {
+            file := int(line[i]-byte('0'))
+            var space int
+            if i == len(line)-1 {
+                space = 0
+            } else {
+                space = int(line[i+1]-byte('0'))
+            }
+            for i := 0; i<file; i++ {
+                fs = append(fs, id)
+            }
+            for i := 0; i<space; i++ {
+                fs = append(fs, -1)
+            }
+            id++
+        }
+
+        N := len(fs)-1
+        for i := 0; i<len(fs); i++ {
+            if fs[i] != -1 {
+                continue
+            }
+            
+            for fs[N] == -1 {
+                N--
+            }
+            if N <= i {
+                break
+            }
+
+            fs[i] = fs[N]
+            fs[N] = -1
+            N--
+        }
+
+        for i, x := range fs {
+            if x == -1 {
+                continue
+            }
+            ans += i*x
+        }
 	}
 }
--- day9/part2.go
+++ day9/part2.go
@@ -3,16 +3,78 @@ package day9
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part2(inpt io.Reader) string {
 	reader := bufio.NewReader(inpt)
+    fs := make([]int,0)
+    type file struct {
+        size int
+        pos int
+        id int
+    }
+    files := make([]file,0)
+    spaces := make([]file,0)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        isFile := true
+        id := 0
+        for _, ch := range line {
+            if isFile {
+                n := int(ch-rune('0'))
+                files = append(files, file{n,len(fs),id})
+                for i:=0; i<n; i++ {
+                    fs = append(fs, id)
+                }
+                id++
+            } else {
+                n := int(ch-rune('0'))
+                spaces = append(spaces, file{n,len(fs),-1})
+                for i:=0; i<n; i++ {
+                    fs = append(fs, -1)
+                }
+            }
+            isFile = !isFile
+        }
 	}
+
+    for i:=len(files)-1; i>=0; i-- {
+        for j:=0; j<len(spaces); j++ {
+            if spaces[j].size == 0 {
+                continue
+            }
+            if spaces[j].pos > files[i].pos {
+                goto nextFile
+            }
+            if spaces[j].size >= files[i].size {
+                for k:=0; k<files[i].size; k++ {
+                    fs[spaces[j].pos+k] = files[i].id
+                    fs[files[i].pos+k] = -1
+                }
+                rem := spaces[j].size-files[i].size
+                spaces[j].size = rem
+                spaces[j].pos += files[i].size
+                goto nextFile
+            }
+        }
+
+nextFile:
+    }
+
+    var ans int
+    for i,x := range fs {
+        if x == -1 {
+            continue
+        }
+        ans += i*x
+    }
+
+    return strconv.Itoa(ans)
 }
