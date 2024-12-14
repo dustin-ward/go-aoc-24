64aecf88dae1b525bf486601e8209a9178333515
dustin-ward <wardddustin@gmail.com>
2024-12-14 01:39:45 -0500

    Day 14 Solutions

--- day14/part1.go
+++ day14/part1.go
@@ -2,17 +2,60 @@ package day14
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 func Part1(inpt io.Reader) string {
+    type xy struct {
+        x int
+        y int
+    }
+    type robot struct {
+        p xy
+        v xy
+    }
+    robots := make([]robot,0)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+        var r robot
+        fmt.Sscanf(line, "p=%d,%d v=%d,%d", &r.p.x, &r.p.y, &r.v.x, &r.v.y)
+        robots = append(robots, r)
 	}
+
+    N := 103
+    M := 101
+
+    Q := make([]int, 4)
+    for _, r := range robots {
+        for _ = range 100 {
+            r.p.x = ((r.p.x + r.v.x) + M) % M
+            r.p.y = ((r.p.y + r.v.y) + N) % N
+        }
+
+        if r.p.x < M/2 {
+            if r.p.y < N/2 {
+                Q[0]++
+            } else if r.p.y > N/2 {
+                Q[1]++
+            }
+        } else if r.p.x > M/2{
+            if r.p.y < N/2 {
+                Q[2]++
+            } else if r.p.y > N/2 {
+                Q[3]++
+            }
+        }
+    }
+
+    ans := Q[0] * Q[1] * Q[2] * Q[3]
+    return strconv.Itoa(ans)
 }
--- day14/part2.go
+++ day14/part2.go
@@ -2,17 +2,56 @@ package day14
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Part2(inpt io.Reader) string {
+    type xy struct {
+        x int
+        y int
+    }
+    type robot struct {
+        p xy
+        v xy
+    }
+    robots := make([]robot,0)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+        var r robot
+        fmt.Sscanf(line, "p=%d,%d v=%d,%d", &r.p.x, &r.p.y, &r.v.x, &r.v.y)
+        robots = append(robots, r)
 	}
+
+    N := 103
+    M := 101
+
+    for iter := 0; iter < 10000; iter++ {
+        B := make([][]rune,N)
+        for i := range N {
+            B[i] = make([]rune,M)
+        }
+        for i, _ := range robots {
+            robots[i].p.x = ((robots[i].p.x + robots[i].v.x) + M) % M
+            robots[i].p.y = ((robots[i].p.y + robots[i].v.y) + N) % N
+            B[robots[i].p.y][robots[i].p.x] = 'X'
+        }
+
+        for _, r := range B {
+            if strings.Contains(string(r), "XXXXXXXXXXXXXXXXXXXX") {
+                return strconv.Itoa(iter+1)
+            }
+        }
+    }
+
+    return strconv.Itoa(0)
 }
