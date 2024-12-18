29bb59dfef96d7fd82549f3a6bdcf9de4e377356
dustin-ward <wardddustin@gmail.com>
2024-12-18 16:52:53 -0500

    Day 18 Pt2 to UnionFind

--- day18/part2.go
+++ day18/part2.go
@@ -13,8 +13,6 @@ func Part2(inpt io.Reader) string {
     var W int = 71
     var H int = 71
 
-    var BYTES int = 1024
-
     board := make([][]byte,H)
     for i := range H {
         board[i] = []byte(strings.Repeat(".", W))
@@ -26,7 +24,6 @@ func Part2(inpt io.Reader) string {
     }
     bytes := make([]xy,0)
 
-    linesRead := 0
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
@@ -39,17 +36,14 @@ func Part2(inpt io.Reader) string {
         fmt.Sscanf(line, "%d,%d", &x, &y)
 
         bytes = append(bytes, xy{x,y})
-
-        if linesRead == BYTES {
-            continue
-        }
         board[y][x] = '#'
-        linesRead++
 	}
+
+    UF := complibgo.NewUnionFind(W*H)
 
     var dy = []int{0,-1,0,1}
     var dx = []int{-1,0,1,0}
-    
+
     bounds := func(i,j int)bool {
         return i>=0&&i<H&&j>=0&&j<W
     }
@@ -58,34 +52,37 @@ func Part2(inpt io.Reader) string {
         return (i*W)+j
     }
 
-    var ans xy
-    for idx := BYTES; idx < len(bytes); idx ++ {
-        byte := bytes[idx]
-        board[byte.y][byte.x] = '#'
-
-        G := complibgo.NewGraphLst(W*H)
-        for i, arr := range board {
-            for j, ch := range arr {
-                if ch == '#' {
-                    continue
-                }
-
-                for k := 0; k<4; k++ {
-                    i2 := i+dy[k]
-                    j2 := j+dx[k]
-                    if bounds(i2,j2) && board[i2][j2] != '#' {
-                        G.AddEdge(getIdx(i,j), getIdx(i2,j2), 1)
-                    }
+    for i, arr := range board {
+        for j := range arr {
+            if board[i][j] == '#' {
+                continue
+            }
+            for k := 0; k<4; k++ {
+                i2 := i+dy[k]
+                j2 := j+dx[k]
+                if bounds(i2,j2) && board[i2][j2] != '#' {
+                    UF.Merge(getIdx(i,j), getIdx(i2,j2))
                 }
             }
         }
+    }
 
-        D, _ := complibgo.Dijkstra_Sparse(G, 0)
-        if D[(W*H)-1] == complibgo.INF {
-            ans = byte
-            break
+    byteIdx := len(bytes)
+    for byteIdx >= 0 && UF.Find(getIdx(0,0)) != UF.Find(getIdx(H-1,W-1)) {
+        byteIdx--
+        
+        x,y := bytes[byteIdx].x, bytes[byteIdx].y
+        board[y][x] = '.'
+
+        for k := 0; k < 4; k++ {
+            x2 := x+dx[k]
+            y2 := y+dy[k]
+            if bounds(x2,y2) && board[y2][x2] != '#' {
+                UF.Merge(getIdx(y,x), getIdx(y2,x2))
+            }
         }
     }
 
+    ans := bytes[byteIdx]
     return fmt.Sprintf("%d,%d", ans.x, ans.y)
 }
