9b7b99a459d5975579db762be13df15a3887b994
dustin-ward <wardddustin@gmail.com>
2024-12-19 00:26:17 -0500

    Day 19 Solutions

--- day19/part1.go
+++ day19/part1.go
@@ -3,16 +3,48 @@ package day19
 import (
 	"bufio"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Part1(inpt io.Reader) string {
 	reader := bufio.NewReader(inpt)
+    line, _ := reader.ReadString('\n')
+	line = line[:len(line)-1]
+
+    towels := make(map[string]struct{})
+    for _, str := range strings.Split(line, ",") {
+        towels[strings.Trim(str, " ")] = struct{}{}
+    }
+    line, _ = reader.ReadString('\n')
+
+    var f func(str string)bool
+    f = func(str string)bool {
+        if str == "" {
+            return true
+        }
+
+        canDo := false
+        for k, _ := range towels {
+            if strings.HasPrefix(str, k) {
+                canDo = canDo || f(str[len(k):])
+            }
+        }
+
+        return canDo
+    }
+
+    var ans int
 	for {
-		line, err := reader.ReadString('\n')
+        line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            return strconv.Itoa(ans)
 		}
-		line = line[:len(line)-1]
+        line = line[:len(line)-1]
+
+        if f(line) {
+            ans++
+        }
 	}
 }
--- day19/part2.go
+++ day19/part2.go
@@ -3,16 +3,52 @@ package day19
 import (
 	"bufio"
 	"io"
+	"strconv"
+	"strings"
 )
 
 func Part2(inpt io.Reader) string {
 	reader := bufio.NewReader(inpt)
+    line, _ := reader.ReadString('\n')
+	line = line[:len(line)-1]
+
+    towels := make(map[string]struct{})
+    for _, str := range strings.Split(line, ",") {
+        towels[strings.Trim(str, " ")] = struct{}{}
+    }
+    line, _ = reader.ReadString('\n')
+
+    memo := make(map[string]int)
+    var f func(str string)int
+    f = func(str string)int {
+        if str == "" {
+            return 1
+        }
+
+        if v, ok := memo[str]; ok {
+            return v
+        }
+
+        ans := 0
+        for k, _ := range towels {
+            if strings.HasPrefix(str, k) {
+                ans +=f(str[len(k):])
+            }
+        }
+
+        memo[str] = ans
+        return ans
+    }
+
+    var ans int
 	for {
-		line, err := reader.ReadString('\n')
+        line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            return strconv.Itoa(ans)
 		}
-		line = line[:len(line)-1]
+        line = line[:len(line)-1]
+
+        ans += f(line)
 	}
 }
