d8a181dc0686b28df2473b67447320b5ef2d8367
dustin-ward <wardddustin@gmail.com>
2024-12-22 00:39:47 -0500

    Day 22 Solutions

--- day22/part1.go
+++ day22/part1.go
@@ -3,16 +3,43 @@ package day22
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part1(inpt io.Reader) string {
+    buyers := make([]int,0)
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+        x, _ := strconv.Atoi(line)
+        buyers = append(buyers, x)
 	}
+
+    const MOD = 16777216
+
+    for _ = range 2000 {
+        for i := range buyers {
+            x := buyers[i] * 64           
+            buyers[i] ^= x
+            buyers[i] = buyers[i] % MOD
+            x = buyers[i] / 32
+            buyers[i] ^= x
+            buyers[i] = buyers[i] % MOD
+            x = buyers[i] * 2048
+            buyers[i] ^= x
+            buyers[i] = buyers[i] % MOD
+        }
+    }
+
+    var ans int
+    for _, x := range buyers {
+        ans += x
+    }
+
+    return strconv.Itoa(ans)
 }
--- day22/part2.go
+++ day22/part2.go
@@ -3,16 +3,73 @@ package day22
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 func Part2(inpt io.Reader) string {
+    buyers := [][2001]int{}
+    prices := [][2001]int{}
+    delta := [][2001]int{}
 	reader := bufio.NewReader(inpt)
+    bIdx := 0
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+        x, _ := strconv.Atoi(line)
+        buyers = append(buyers, [2001]int{})
+        prices = append(prices, [2001]int{})
+        delta = append(delta, [2001]int{})
+        buyers[bIdx][0] = x
+        prices[bIdx][0] = x%10
+        delta[bIdx][0] = 0
+        bIdx++
 	}
+
+    const MOD = 16777216
+
+    for i := range buyers {
+        for j := 1; j <= 2000; j++ {
+            temp := buyers[i][j-1]
+            x := temp * 64           
+            temp ^= x
+            temp = temp % MOD
+            x = temp / 32
+            temp ^= x
+            temp = temp % MOD
+            x = temp * 2048
+            temp ^= x
+            temp = temp % MOD
+
+            buyers[i][j] = temp
+            prices[i][j] = temp % 10
+            delta[i][j] = prices[i][j] - prices[i][j-1]
+        }
+    }
+
+    ansMap := make(map[string]int)
+    for i := range buyers {
+        buyersMap := make(map[string]bool)
+        for j := 1; j<=2000-3; j++ {
+            var key string
+            key += strconv.Itoa(delta[i][j]) + ","
+            key += strconv.Itoa(delta[i][j+1]) + ","
+            key += strconv.Itoa(delta[i][j+2]) + ","
+            key += strconv.Itoa(delta[i][j+3])
+
+            if _, ok := buyersMap[key]; !ok {
+                buyersMap[key] = true
+                ansMap[key] += prices[i][j+3]
+            }
+        }
+    }
+
+    var ans int
+    for _, v := range ansMap {
+        ans = max(ans, v)
+    }
+    return strconv.Itoa(ans)
 }
