0681244444b75c1d1d614a3148a01e7c52c17aef
dustin-ward <wardddustin@gmail.com>
2024-12-23 14:11:01 -0500

    Day 23 Solutions

--- day23/part1.go
+++ day23/part1.go
@@ -2,17 +2,65 @@ package day23
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func Part1(inpt io.Reader) string {
+    adj := make(map[string][]string)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        s := strings.Split(line, "-")
+        c1 := s[0]
+        c2 := s[1]
+
+        if _, ok := adj[c1]; !ok {
+            adj[c1] = make([]string,0)
+        }
+        if _, ok := adj[c2]; !ok {
+            adj[c2] = make([]string,0)
+        }
+
+        adj[c1] = append(adj[c1], c2)
+        adj[c2] = append(adj[c2], c1)
 	}
+
+    ans := make(map[string]struct{})
+    for i,v := range adj {
+        if i[0] != 't' {
+            continue
+        }
+
+        for _, j := range v {
+            if j == i {
+                continue
+            }
+            for _, k := range adj[j] {
+                if k == i || k == j {
+                    continue
+                }
+                for _, x := range adj[k] {
+                    if x == i {
+                        c := []string{i,j,k}
+                        slices.Sort(c)
+                        ans[fmt.Sprintf("%s,%s,%s", c[0],c[1],c[2])] = struct{}{}
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    return strconv.Itoa(len(ans))
 }
--- day23/part2.go
+++ day23/part2.go
@@ -3,16 +3,97 @@ package day23
 import (
 	"bufio"
 	"io"
+	"slices"
+	"sort"
+	"strings"
 )
 
 func Part2(inpt io.Reader) string {
+    adj := make(map[string][]string)
+
 	reader := bufio.NewReader(inpt)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
 			//EOF
-			return "Not implemented"
+            break
 		}
 		line = line[:len(line)-1]
+
+        s := strings.Split(line, "-")
+        c1 := s[0]
+        c2 := s[1]
+
+        if _, ok := adj[c1]; !ok {
+            adj[c1] = make([]string,0)
+        }
+        if _, ok := adj[c2]; !ok {
+            adj[c2] = make([]string,0)
+        }
+
+        adj[c1] = append(adj[c1], c2)
+        adj[c2] = append(adj[c2], c1)
 	}
+
+    cliques := make([]map[string]struct{},0)
+
+    var bronKerbosch func(R,P,X map[string]struct{})
+    bronKerbosch = func(R,P,X map[string]struct{}) {
+        if len(P) == 0 && len(X) == 0 {
+            cliques = append(cliques, R)
+            return
+        }
+
+        for len(P) > 0 {
+            var v string
+            for k,_ := range P {
+                v = k
+                break
+            }
+
+            newR := make(map[string]struct{})
+            for k,v := range R {
+                newR[k] = v
+            }
+            newR[v] = struct{}{}
+            newP := make(map[string]struct{})
+            for k, _ := range P {
+                if slices.Index(adj[v], k) != -1 {
+                    newP[k] = struct{}{}
+                }
+            }
+            newX := make(map[string]struct{})
+            for k, _ := range X {
+                if slices.Index(adj[v], k) != -1 {
+                    newX[k] = struct{}{}
+                }
+            }
+
+            bronKerbosch(newR,newP,newX)
+            delete(P, v)
+            X[v] = struct{}{}
+        }
+    }
+    R := make(map[string]struct{})
+    P := make(map[string]struct{})
+    X := make(map[string]struct{})
+    for k := range adj {
+        P[k] = struct{}{}
+    }
+    bronKerbosch(R,P,X)
+
+    sort.Slice(cliques, func(i,j int)bool{
+        return len(cliques[i])>len(cliques[j])
+    })
+    c := make([]string,0)
+    for k := range cliques[0] {
+        c = append(c,k)
+    }
+    slices.Sort(c)
+    ans := ""
+    for _,k := range c {
+        ans += k + ","
+    }
+
+    return ans[:len(ans)-1]
 }
